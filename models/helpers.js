e7c4da63bb9717432c83f28b0c59f4faf9557eea
Jeno13 <yoyo20012001@gmail.com>
2024-07-11 14:47:48 +0800

    Refactored helper function

--- models/helpers.js
+++ models/helpers.js
@@ -1,24 +1,56 @@
 const getCategories = async () => {
-    return await Category.find();
-  };
-  
-  const handleFileUpload = (file) => {
-    if (file) {
-      file.mv('./public/uploads/' + file.name);
-      return file.name;
-    }
-    return '';
-  };
-  
-  const deleteFile = (filePath) => {
-    if (filePath && fs.existsSync(filePath)) {
-      fs.unlinkSync(filePath);
-    }
-  };
-  
-  module.exports = {
-    getCategories,
-    handleFileUpload,
-    deleteFile,
-  };
-  
+  return await Category.find();
+};
+
+const getUserProjects = async (userId) => {
+  return await Project.aggregate([
+    {
+      $match: {
+        user: mongoose.Types.ObjectId.createFromHexString(userId),
+      },
+    },
+    {
+      $lookup: {
+        from: "categories",
+        localField: "category",
+        foreignField: "_id",
+        as: "category",
+      },
+    },
+    {
+      $unwind: "$category",
+    },
+    {
+      $lookup: {
+        from: "requests",
+        localField: "_id",
+        foreignField: "project",
+        as: "requests",
+      },
+    },
+    {
+      $sort: { createdAt: -1 },
+    },
+  ]);
+};
+
+const handleFileUpload = (file) => {
+  if (file) {
+    file.mv("./public/uploads/" + file.name);
+    return file.name;
+  }
+  return "";
+};
+
+const deleteFile = (filePath) => {
+  if (filePath && fs.existsSync(filePath)) {
+    fs.unlinkSync(filePath);
+  }
+};
+
+module.exports = {
+  getCategories,
+  getUserProjects,
+  handleFileUpload,
+  deleteFile,
+};
